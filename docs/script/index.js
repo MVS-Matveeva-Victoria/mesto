ef1f0b9e5d3b8259591c045b71f27741b50688ee
Victoria Matveeva <VictoriaMtv@yandex.ru>
2023-01-30 05:42:40 +0300

    исправить js файл, удалить высоту header, отцентрировать popup во вертикале на 320px

--- docs/script/index.js
+++ docs/script/index.js
@@ -1,53 +1,43 @@
 "use strict";
 
 // Для открытия popup
-let profileRedactButton = document.querySelector('.profile__redact');
-let popup = document.querySelector('.popup');
+let profileRedactButton = document.querySelector(".profile__redact");
+let popup = document.querySelector(".popup");
 
-//для переноса значения из профиля в инпуты 
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-let popupInputTitleValue = document.querySelector('.popup__input_title_value');
-let popupInputSubtitleValue = document.querySelector('.popup__input_subtitle_value');
+//для переноса значения из профиля в инпуты
+let profileTitle = document.querySelector(".profile__title");
+let profileSubtitle = document.querySelector(".profile__subtitle");
+let popupInputTitleValue = document.querySelector(".popup__input_title_value");
+let popupInputSubtitleValue = document.querySelector(".popup__input_subtitle_value");
 
 //для закрытия попапа
-let popupEditButton = document.querySelector('.popup__edit-button');
+let popupEditButton = document.querySelector(".popup__edit-button");
 
 // для сохранения данных в профиль
-let saveButton = document.querySelector('.popup__button');
+let formElement = document.querySelector(".popup__form-save");
 
-
-document.querySelector('.profile__redact').addEventListener('click', function() {
-	popup.classList.add('popup_opened');
+profileRedactButton.addEventListener("click", function () {
+  popup.classList.add("popup_opened");
   popupInputTitleValue.value = profileTitle.textContent;
   popupInputSubtitleValue.value = profileSubtitle.textContent;
 });
 
-
-function saveForm(event) {
+formElement.addEventListener("submit", function (event) {
   event.preventDefault();
-	profileTitle.textContent = popupInputTitleValue.value;
-	profileSubtitle.textContent = popupInputSubtitleValue.value;
-	
-	popup.classList.remove('popup_opened');
-}
+  profileTitle.textContent = popupInputTitleValue.value;
+  profileSubtitle.textContent = popupInputSubtitleValue.value;
+  popupEdit();
+});
 
 function popupEdit() {
-	popupInputTitleValue.value = profileTitle.textContent;
-  popupInputSubtitleValue.value = profileSubtitle.textContent;
-	
-	popup.classList.remove('popup_opened');
+  popup.classList.remove("popup_opened");
 }
 
-saveButton.addEventListener('click', function(event) {
-  saveForm(event);
-});
-
-popupEditButton.addEventListener('click', popupEdit);
-
-//функция для лайков
-document.querySelectorAll('.places__like').forEach((element) => {
-  element.addEventListener('click', function() {
-  element.classList.toggle('places__like_active');
-  });
-});
+//функция для лайков к следующему спринту
+//document.querySelectorAll(".places__like").forEach((element) => {
+//  element.addEventListener("click", function () {
+//    element.classList.toggle("places__like_active");
+//  });
+//});
+//
+//popupEditButton.addEventListener("click", popupEdit);
